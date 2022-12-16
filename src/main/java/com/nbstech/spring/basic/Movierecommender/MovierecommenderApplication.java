9995d4aa3bedcc51e8ad790e5741bcac0d1b7fa5
tunguyen <tunguyenkawa@gmail.com>
2022-12-16 16:43:39 +0700

    Init generated by Spring Initializer

---
+++ src/main/java/com/nbstech/spring/basic/Movierecommender/MovierecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.nbstech.spring.basic.Movierecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierecommenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nbstech/spring/basic/Movierecommender/MovierecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbstech.spring.basic.Movierecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
